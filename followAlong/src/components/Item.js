c463e531cdd9fd40eeb7923ba04dbd36094a5ee1
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-05-03 13:33:55 -0400

    lecture work-in-progress

--- followAlong/src/components/Item.js
+++ followAlong/src/components/Item.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Item = props => {
+  const [item, setItem] = useState({
+    name: 'Bananas',
+    id: 123,
+    purchased: false
+  });
+
   const handleClick = ()=> {
-    console.log("clicked ", props.item.name);
+    console.log("clicked ", item.name);
   }
-  
+
   return (
-    <div onClick={handleClick} className={`item${props.item.purchased ? ' purchased' : ''}`}>
-      <p>{props.item.name}</p>
+    <div onClick={handleClick} className={`item${item.purchased ? ' purchased' : ''}`}>
+      <p>{item.name}</p>
     </div>
   );
 };
