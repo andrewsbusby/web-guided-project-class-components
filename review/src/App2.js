c7fa276a7125044e9eee97d7eaa2916c8422e7b0
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-05-03 12:56:35 -0400

    lecture work-in-progress

--- review/src/App2.js
+++ review/src/App2.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 class App2 extends React.Component {
+    constructor() {
+
+    }
     
     render() {
         const name = "Allison";
-    
 
         return(<div>
             <h1>Hello {name}.</h1>
