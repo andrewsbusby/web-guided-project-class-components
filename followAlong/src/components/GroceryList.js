3cebd52762593786e1c10c85f009b2de248b7974
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-05-03 13:59:43 -0400

    lecture work-in-progress

--- followAlong/src/components/GroceryList.js
+++ followAlong/src/components/GroceryList.js
@@ -5,12 +5,17 @@ import Item from "./Item";
 const GroceryList = props => {
   // for sorting the list based on whether an item has been purchased or not
   // const sortedList = props.groceries.sort((a, b) => a.purchased - b.purchased);
+
+  const handleClick = ()=>{
+    
+  }
+
   return (
     <div className="shopping-list">
       {props.groceries.map(item => (
         <Item key={item.id} toggleItem={props.toggleItem} item={item} />
       ))}
-      <button className="clear-btn">
+      <button onClick={handleClick} className="clear-btn">
         Clear Purchased
       </button>
     </div>
