70099d9a7c7506cd1ebf945b3c39047db6780ee6
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-05-03 12:23:48 -0400

    lecture work-in-progress

--- review/src/App.js
+++ review/src/App.js
@@ -12,6 +12,7 @@ const App = () => {
   return (
     <div>
       <h1>Hello {name}. Age is equal to {age}.</h1>
+      <h3>You're {name}!?!? {name} is a fantastic name! Never change it!!</h3>
       <button onClick={handleClick}>Change the State!</button>
     </div>
   );
