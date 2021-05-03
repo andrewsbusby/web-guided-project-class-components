5f01e518c741846c3504093cb1d18054f126dc98
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-05-03 12:22:28 -0400

    lecture work-in-progress

--- review/src/App.js
+++ review/src/App.js
@@ -4,13 +4,15 @@ const App = () => {
   const [name, setName] = useState("Warren");
   const [age, setAge] = useState(33);
 
+  const handleClick = ()=>{
+    setName("Allison");
+    setAge(22);
+  }
+  
   return (
     <div>
       <h1>Hello {name}. Age is equal to {age}</h1>
-      <button onClick={()=>{
-        setName("Allison");
-        setAge(22);
-      }}>Change the State!</button>
+      <button onClick={handleClick}>Change the State!</button>
     </div>
   );
 };
