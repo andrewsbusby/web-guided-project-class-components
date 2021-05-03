cd37f46d4d7ec9e447c37eee78148156d7a8084b
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-05-03 13:01:15 -0400

    lecture work-in-progress

--- review/src/App2.js
+++ review/src/App2.js
@@ -11,12 +11,8 @@ class App2 extends React.Component {
 
     render() {
         return(<div>
-            <h1>Hello {this.state.name}. Age is {this.state.age}</h1>
-            <button onClick={()=>{
-                this.setState({
-                    name: "Allison"
-                });
-            }}>Change the state</button>
+            <h1>Hello {this.state.name}. Age is {this.state.age}.</h1>
+            <button onClick={handleClick}>Change the state</button>
         </div>);
     }
 }
