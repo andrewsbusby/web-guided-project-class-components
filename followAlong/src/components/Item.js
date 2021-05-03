c106bd0454482915ff233d3ccfd0170429b6ffe7
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-05-03 13:40:09 -0400

    lecture work-in-progress

--- followAlong/src/components/Item.js
+++ followAlong/src/components/Item.js
@@ -1,18 +1,19 @@
 import React, {useState} from 'react';
 
 const Item = props => {
-  const [item, setItem] = useState({
-    name: 'Bananas',
-    id: 123,
-    purchased: false
-  });
+  // const [item, setItem] = useState({
+  //   name: 'Bananas',
+  //   id: 123,
+  //   purchased: false
+  // });
 
   const handleClick = ()=> {
-    console.log("clicked ", item.name);
-    setItem({
-      ...item,
-      purchased: !item.purchased
-    })
+    props.toggleItem(item.id);
+
+    // setItem({
+    //   ...item,
+    //   purchased: !item.purchased
+    // })
   }
 
   return (
