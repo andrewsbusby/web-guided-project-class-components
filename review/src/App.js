50df193c5181a196875b27b96ebb1141791cd5b8
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-05-03 12:26:29 -0400

    lecture work-in-progress

--- review/src/App.js
+++ review/src/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 
-const Person = () => {
+const Person = (props) => {
+  console.log(props);
   return(<div>
-      <h1>Hello {name}. Age is equal to {age}.</h1>
+      <h1>Hello {props.name}. Age is equal to {age}.</h1>
       <h3>You're {name}!?!? {name} is a fantastic name! Never change it!!</h3>
   </div>);
 }
@@ -18,7 +19,7 @@ const App = () => {
 
   return (
     <div>
-      <Person/>
+      <Person name={name} age={age}/>
       <button onClick={handleClick}>Change the State!</button>
     </div>
   );
