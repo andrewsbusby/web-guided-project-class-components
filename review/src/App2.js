1ee74550cdd200f80f8738035e4fbbfbf39b854e
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-05-28 13:56:42 -0600

    update to app2

--- review/src/App2.js
+++ review/src/App2.js
@@ -1,5 +1,23 @@
 import React from 'react';
 
+// const Person = (props) =>{
+//     return(
+//         <div>
+//         <h1>Hello {props.name}. Age is {props.age}.</h1>
+//         <h3>{props.name} is a fine name for you.</h3>
+//         </div>
+//     )
+// }
+
+class Person extends React.Component {
+    render(){
+        return( 
+                <div>
+                    <h1>Hello {this.props.name}. Age is {this.props.age}.</h1>
+                    <h3>{this.props.name} is a fine name for you.</h3>
+                </div>)
+    }
+}
 class App2 extends React.Component {
     constructor() {
         super();
@@ -19,7 +37,7 @@ class App2 extends React.Component {
 
         return(
             <>
-            <div>Hello {this.state.name}. Age is {this.state.age}.</div>
+            <Person name={this.state.name} age={this.state.age} />
             <button onClick={this.handleClick}>Change the state here!</button>
             </>
         )
