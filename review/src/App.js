382e1486e47059759d18591c224070e61e8255c6
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-05-28 12:40:06 -0600

    update to review

--- review/src/App.js
+++ review/src/App.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 
 const App = () => {
+  const [name, setName] = useState('Andrew');
+  const [age, setAge] = useState(32);
   return (
     <div>
-      <h1>Hello Warren</h1>
+      <h1>Hello {name}. Age is equal to {age}</h1>
     </div>
   );
 };
