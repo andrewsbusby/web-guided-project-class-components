33a7bd13b33c43c77400b45939942495ff84387b
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-05-28 13:00:25 -0600

    review of unit 2

--- review/src/App.js
+++ review/src/App.js
@@ -1,11 +1,25 @@
 import React, { useState } from "react";
 
+const Person = (props) =>{
+  return(
+    <div>
+      <h1>Hello {props.name}. Age is equal to {props.age}</h1>
+      <h3>You're {props.name}!? {props.name} is a fantastic Name. Never change it.</h3>
+    </div>
+  )
+}
 const App = () => {
   const [name, setName] = useState('Andrew');
   const [age, setAge] = useState(32);
+  const handleChange = ()=>{
+    setName('Busby');
+    setAge(16);
+  }
+
   return (
     <div>
-      <h1>Hello {name}. Age is equal to {age}</h1>
+      <Person name={name} age={age}/>
+      <button onClick={handleChange}>Change the State!</button>
     </div>
   );
 };
